e6d6df0ddc947437cca97160f76569dde5be6e6e
nofellings <djleonardhd@hotmail.com>
2021-10-23 06:06:37 +0000

    💻 add development/production function and more changes

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,12 +1,17 @@
 import { Client, ClientOptions } from "discord.js";
+import EventHandler from "../handlers/EventHandler";
+import { config as configure } from "dotenv";
 
-export class Bot extends Client {
-
+export default class Bot extends Client {
   constructor(options: ClientOptions) {
     super(options);
   }
-
-  async login(token?: string) {
-    return await super.login(token);
+  private getToken(): string | undefined {
+    return process.env.PRODUCTION ? process.env.BOT_TOKEN : process.env.TEST_TOKEN;
+  }
+  public startSystem(): void {
+    configure();
+    EventHandler.load("../events", this);
+    this.login(this.getToken());
   }
 }
