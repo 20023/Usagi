d09dc0f7b7f0143f1eaca5237a6d415d6a1b921f
nofellings <djleonardhd@hotmail.com>
2021-10-24 20:50:45 +0000

    ❌ Removing unecessary require

--- src/handlers/CommandHandler.ts
+++ src/handlers/CommandHandler.ts
@@ -4,19 +4,14 @@ import BaseCommand from "../structures/BaseCommand";
 
 export default new class CommandHandler {
     load(mainPath: string, subPaths: Array<string>, client: Client): void {
-
         for (const path of subPaths) {
             readdir(`${__dirname}/${mainPath}/${path}`, async (err, files) => { 
                 if (err) throw err;
-
                 for (let file of files) {
                     // Import the command.
-                    const { default: Command } = require(`${__dirname}/${mainPath}/${path}/${file}`)
-
-                    // const { default: Command } = await import(file.path);
+                    const { default: Command } = await import(`${__dirname}/${mainPath}/${path}/${file}`)
 
                     const command: BaseCommand = new Command();
-                    // if (command.category !== path) throw new ReferenceError("Command category must be the same as file path");
 
                     // Set the client to use that command.
                     client.commands.set(command.name.toLowerCase(), command);
--- src/handlers/EventHandler.ts
+++ src/handlers/EventHandler.ts
@@ -5,10 +5,10 @@ import BaseEvent from "../structures/BaseEvent";
 
 export default new class EventHandler {
     async load(path: string, client: Client) {
-        readdir(`${__dirname}/${path}`, (err, files) => {
+        readdir(`${__dirname}/${path}`, async (err, files) => {
             if (err) throw err;
-            for (let file of files) {
-                const { default: Event } = require(`${__dirname}/${path}/${file}`)
+            for (const file of files) {
+                const { default: Event } = await import(`${__dirname}/${path}/${file}`)
                 const event = <BaseEvent>new Event()
                 client.on(event.name, event.run.bind(null, client));
             }
