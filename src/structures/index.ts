4cae9fb9372b5b7458101e52e4455f16c417bf42
nofellings <djleonardhd@hotmail.com>
2021-10-24 15:22:32 +0000

    📝Add index with the structures

---
+++ src/structures/index.ts
@@ -0,0 +1,4 @@
+export { default as Client } from "./Client";
+export { default as BaseEvent } from "./BaseEvent";
+export { default as BaseCommand } from "./BaseCommand";
+export { default as EmbedBuilder } from "./EmbedBuilder";
